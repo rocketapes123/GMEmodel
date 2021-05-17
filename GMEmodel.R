115620cc865cd0445367cf9a50600a1d29b47c4a
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-05-17 14:55:54 +0200

    Create GMEmodel.R

---
+++ GMEmodel.R
@@ -0,0 +1,7 @@
+data<-read.csv2("LMGME.csv")
+data$Date<-NULL
+data$FTD<-as.factor(data$FTD)
+data$Sett<-as.factor(data$Sett)
+data$Weekday<-as.factor(data$Weekday)
+lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC, data=data)
+summary(lmGME)
