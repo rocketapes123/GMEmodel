3f506f7d49231cce649c71ddf6e89e200a629830
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-05-18 09:38:26 +0200

    Update Predict.R
    updates variables

--- Predict.R
+++ Predict.R
@@ -1,10 +1,11 @@
-###### Input the following values pro prediction of this day: "Weekday","Sett","Volume1HPM","Return1H","FTD","Beta.3M","Beta4W","Beta2W","Beta1W","BTC","MaxPain"
-input <- as.matrix(c(4,-1,2298,0.27,13,2.95,0.3,1.06,1.38,-13.32,-1.61)) #### Values as of Thursday, 2021/05/13
-input <- as.matrix(c(5,0,3150,1.24,14,5,3.11,1.33,2.77,3.53,1.15,1.64)) ### Values as of Friday, 2021/05/14
-input <- as.matrix(c(1,1,1138,1.09,15,2.81,1.3,1.88,2.26,-0.65,9.68)) #### Values as of Monday, 2021/05/17
-
+###### Input the following values pro prediction of this day: "Weekday","Sett","Volume1HPM","Return1H","FTD","Beta.3M","Beta4W","Beta2W","Beta1W","BTC","MaxPain","RGME_PD","RAMC_PD","ReturnAMPD"
+input <- as.matrix(c(4,-1,2298,0.27,13,2.95,0.3,1.06,1.38,-13.32,-1.61,-1.45,2.69,0.91)) #### Values as of Thursday, 2021/05/13
+input <- as.matrix(c(5,0,3150,1.24,14,5,3.11,1.33,2.77,3.53,1.15,1.64,13.61,23.74,-1.82)) ### Values as of Friday, 2021/05/14
+input <- as.matrix(c(1,1,1138,1.09,15,2.81,1.3,1.88,2.26,-0.65,9.68,-2.78,1.64,0.36)) #### Values as of Monday, 2021/05/17
+input <- as.matrix(c(2,2,1138,1.09,16,2.721,1.62,1.79,2.47,-6.28,0,12.93,7.47,-0.89)) #### Values as of Tuesday, 2021/05/18
 datainput<-as.data.frame(t(input))
-names(datainput)<-c("Weekday","Sett","Volume1HPM","Return1H","FTD","Beta.3M","Beta4W","Beta2W","Beta1W","BTC","MaxPain")
+names(datainput)<-c("Weekday","Sett","Volume1HPM","Return1H","FTD","Beta.3M","Beta4W","Beta2W","Beta1W","BTC",
+                    "MaxPain","RGME_PD","RAMC_PD","ReturnAMPD")
 datainput$FTD<-as.factor(datainput$FTD)
 datainput$Sett<-as.factor(datainput$Sett)
 datainput$Weekday<-as.factor(datainput$Weekday)
