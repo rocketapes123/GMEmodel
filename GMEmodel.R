902b056d00dc17a2d4463a08b7a55458e94d8a70
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-05-19 12:10:51 +0200

    Update GMEmodel.R
    added TenY treasury and SPY

--- GMEmodel.R
+++ GMEmodel.R
@@ -4,5 +4,5 @@ data$FTD<-as.factor(data$FTD)
 data$Sett<-as.factor(data$Sett)
 data$Weekday<-as.factor(data$Weekday)
 lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC+MaxPain
-          +RGME_PD+RAMC_PD+ReturnAMPD, data=data)
+          +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY, data=data)
 summary(lmGME)
