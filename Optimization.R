a4430296d9582c180fc2a6af9106bcdac88fdc37
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-05-30 20:33:55 +0200

    Update Optimization.R

--- Optimization.R
+++ Optimization.R
@@ -2,44 +2,7 @@
 simulation<- function(data, lmGME, prints=F, n, delta)
 {
   
-  analysestart<-as.data.table(add_predictions(as.data.frame(data),lmGME))
-  analysestart$ÜRichtung<-ifelse(sign(analysestart$ReturnGME)==sign(analysestart$pred),1,0)
-  analysestart$Fehler<-(analysestart$pred-analysestart$ReturnGME)
-  Gütestart <- (1-mean(analysestart$ÜRichtung))*50+mean(abs(analysestart$Fehler))
-  coefs<-lmGME$coefficients
-  lmGMEt<-lmGME
-  for( i in 1:n)
-    
-  {
-    rand1<-floor(runif(1,1, length((coefs))+1))
-    rand2<-floor(runif(1,1, length((coefs))+1))
-    
-    randv1<-runif(1,-delta,delta)
-    randv2<-runif(1,-delta,delta)
-    
-    lmGMEt$coefficients[rand1]<-lmGMEt$coefficients[rand1]*(1+randv1)
-    lmGMEt$coefficients[rand2]<-lmGMEt$coefficients[rand2]*(1+randv2)
-    #if(prints==T){print(lmGMEt$coefficients[rand1]*(1+randv1))}
-    #if (prints==T){print(lmGMEt$coefficients[rand2]*(1+randv2))}
-    analysetmp<-as.data.table(add_predictions(as.data.frame(data),lmGMEt))
-    analysetmp$ÜRichtung<-ifelse(sign(analysetmp$ReturnGME)==sign(analysetmp$pred),1,0)
-    analysetmp$Fehler<-(analysetmp$pred-analysetmp$ReturnGME)
-    Güte <- (1-mean(analysetmp$ÜRichtung))*50+mean(abs(analysetmp$Fehler))
-    if(prints==T){print(Güte)}
-    if (Güte <= Gütestart)
-      
-    {
-      lmGME<-lmGMEt
-      print(c(i,Gütestart,Güte,names(lmGMEt$coefficients[rand1])
-            ,names(lmGMEt$coefficients[rand2]))
-            )
-      Gütestart<-Güte
-    } else {
-      
-      lmGMEt<-lmGME
-    }
-  }
-  
+####removed
   
  return(lmGME) 
 }
