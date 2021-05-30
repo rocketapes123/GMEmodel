c35b85d00ada9aca3b648cf9372c8a250f353cf7
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-05-30 20:33:23 +0200

    Update GMEmodel.R

--- GMEmodel.R
+++ GMEmodel.R
@@ -3,6 +3,5 @@ data$Date<-NULL
 data$FTD<-as.factor(data$FTD)
 data$Sett<-as.factor(data$Sett)
 data$Weekday<-as.factor(data$Weekday)
-lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC+MaxPain
-          +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY, data=data)
+########removed
 summary(lmGME)
