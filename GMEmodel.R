4225e27b81f5c4bf6e1c4bd41626172fbdef3a3b
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-06-01 08:54:15 +0200

    Update GMEmodel.R

--- GMEmodel.R
+++ GMEmodel.R
@@ -1,7 +1,15 @@
-data<-read.csv2("LMGME.csv")
+library("data.table")
+library("modelr")
+
+
+data<-read.csv2("LMGME.csv") ### or LMGME_e.csv
 data$Date<-NULL
 data$FTD<-as.factor(data$FTD)
 data$Sett<-as.factor(data$Sett)
 data$Weekday<-as.factor(data$Weekday)
-########removed
-summary(lmGME)
+data$RCTweetPD<-as.factor(data$RCTweetPD)
+#data<-data[data$FTD!=21,]
+#data<-data[data$FTD!=1,]
+#data<-data[data$Volume1HPM<10000,]
+lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC+MaxPain
+          +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY+RCTweetPD+RSIPD, data=data)
