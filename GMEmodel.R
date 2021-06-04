4a4c3c75713ff65c4a57168b6e047e5d2380ed33
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-06-04 13:04:15 +0200

    Update GMEmodel.R
    added aditional variables

--- GMEmodel.R
+++ GMEmodel.R
@@ -12,4 +12,5 @@ data$RCTweetPD<-as.factor(data$RCTweetPD)
 #data<-data[data$FTD!=1,]
 #data<-data[data$Volume1HPM<10000,]
 lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC+MaxPain
-          +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY+RCTweetPD+RSIPD, data=data)
+          +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY+RCTweetPD+RSIPD+SP1H+AMC1H+MACDHISTPD
+          +mPlastPrice, data=data)
