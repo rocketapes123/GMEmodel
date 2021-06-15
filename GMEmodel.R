d370b0be0e1d8367a1232d752bd8e0b26b993d25
rocketapes123 <84327474+rocketapes123@users.noreply.github.com>
2021-06-15 08:56:13 +0200

    Update GMEmodel.R

--- GMEmodel.R
+++ GMEmodel.R
@@ -13,6 +13,6 @@ data$RCTweetPD<-as.factor(data$RCTweetPD)
 #data<-data[data$Volume1HPM<10000,]
 lmGME<-lm(ReturnGME~Sett+Volume1HPM+Return1H+FTD+Weekday+Beta.3M+Beta4W+Beta2W+Beta1W+BTC+MaxPain
           +RGME_PD+RAMC_PD+ReturnAMPD+TenYCPD+ReturnSPY+RCTweetPD+RSIPD+SP1H+AMC1H+MACDHISTPD+EarningsPD+VIXPD
-          +mPlastPrice
+          +mPlastPrice+T21+T35
           +GMEFTDPD+ETFFTDPD ### only available until mid of may, exclude for more complete coverage
           , data=data)
